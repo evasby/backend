78c152ca3e462be77b7303291af08dc98fd62d80
Vladimir Dmitriev <defurator@mail.ru>
2017-02-09 18:05:47 +0200

    header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-
+	//fixed header show
+	$(window).scroll(function() {
+    var top = $(document).scrollTop();
+    if (top < 100) $(".header").removeClass('header__fixed');
+    else $(".header").addClass('header__fixed');
+  });
+  //menu toggle
+  $('.toggle').click(function(){
+    if ($(this).hasClass('toggle__on')) {
+      $(this).removeClass('toggle__on');
+      $(".menu").removeClass('menu__on');
+    } else{
+      $(this).addClass('toggle__on');
+      $(".menu").addClass('menu__on');
+    }
+  });
+  //hide menu
+  $(".menu").click(function(e) {
+    if ($(e.target).closest(".menu_inner").length) return;
+    $('.toggle').removeClass('toggle__on');
+    $(".menu").removeClass('menu__on');
+    e.stopPropagation();
+  });
 });
