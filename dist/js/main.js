0cb3d80c591e6c73744b5658e48abb6fdd50977c
Vladimir Dmitriev <defurator@mail.ru>
2017-02-15 13:51:20 +0200

    eng, dist

---
+++ dist/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	//fixed header show
+	$(window).scroll(function() {
+    var top = $(document).scrollTop();
+    if (top < 250) {
+      $(".header_position").removeClass('header_position__fixed');
+      //$("body").removeClass('fixed');
+    } else {
+      $(".header_position").addClass('header_position__fixed');
+      //$("body").addClass('fixed');
+    }
+  });
+  //menu toggle
+  $('.toggle').click(function(){
+    if ($(this).hasClass('toggle__on')) {
+      $(this).removeClass('toggle__on');
+      $(".menu").removeClass('menu__on');
+    } else{
+      $(this).addClass('toggle__on');
+      $(".menu").addClass('menu__on');
+    }
+  });
+  //hide menu
+  $(".menu").click(function(e) {
+    if ($(e.target).closest(".menu_wrap").length) return;
+    $('.toggle').removeClass('toggle__on');
+    $(".menu").removeClass('menu__on');
+    e.stopPropagation();
+  });
+});
